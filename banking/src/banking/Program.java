a5498211033089459ab8e63cae9e50c95be7b9d5
Soulthym <thymi92@gmail.com>
2017-11-25 20:49:27 +0100

    another desperate attempt...

--- banking/src/banking/Program.java
+++ banking/src/banking/Program.java
@@ -1,9 +1,9 @@
 package banking;
 
-import banking.InputException;
-import banking.IntegerInputChecker;
-import banking.LitteralInputChecker;
-import banking.RuleException;
+// import banking.InputException;
+// import banking.IntegerInputChecker;
+// import banking.LitteralInputChecker;
+// import banking.RuleException;
 
 public class Program {
 
@@ -12,52 +12,87 @@ public class Program {
 	private Account _userAccount;
 
 	public Program () {
-		_interface = new Interface();
-		_operations = new Operations();
+		try {
+			_interface = new Interface();
+			_operations = new Operations();
+		} catch (Exception e) {
+			System.out.println("failed");
+		}
 	}
 
 	public Account connexion () throws RuleException, InputException {
-		int userChoice = Integer.parseInt(_interface.askUser("Hello\nWhat would you like to do? (Fill in with the option's number)\n1) Create an account\n2) Connect to you account", new IntegerInputChecker(1, 2)));
-		String name = _interface.askUser("Please state your name.", new LitteralInputChecker());
-		int age = Integer.parseInt(_interface.askUser("Please enter your age.", new IntegerInputChecker(0, 150)));
+		int userChoice = 0;
+		String name = "";
+		int age = 0;
+		try {
+			userChoice = Integer.parseInt(_interface.askUser("Hello\nWhat would you like to do? (Fill in with the option's number)\n1) Create an account\n2) Connect to you account", new IntegerInputChecker(1, 2)));
+		} catch (Exception e){
+			System.out.println("failed");
+		}
+		try {
+			name = _interface.askUser("Please state your name.", new LitteralInputChecker());
+		} catch (Exception e){
+			System.out.println("failed");
+		}
+		try {
+			age = Integer.parseInt(_interface.askUser("Please enter your age.", new IntegerInputChecker(0, 150)));
+		} catch (Exception e){
+			System.out.println("failed");
+		}
 		switch (userChoice) {
 			case 1 : {
-				_userAccount = _operations.createNewAccount(name, age);
+				try {
+					_userAccount = _operations.createNewAccount(name, age);
+				} catch (Exception e) {
+					System.out.println("failed");
+				}
 				break;
 			}
 			case 2 : {
-				_userAccount = _operations.connectToYourAccount(name);
+				try {
+					_userAccount = _operations.connectToYourAccount(name);
+				} catch (Exception e) {
+					System.out.println("failed");
+				}
 				break;
 			}
 		}
-		_interface.informUser("You are now connected as" + _userAccount.getUserName());
-		return _userAccount;
+		try {
+			_interface.informUser("You are now connected as" + _userAccount.getUserName());
+		} catch (Exception e) {
+			System.out.println("failed");
+		}
+	return _userAccount;
 	}
 
 
 	public void menu (Account account) throws RuleException, InputException {
-		int userChoice = Integer.parseInt(_interface.askUser("What woud you like to do ?(Fill in with the option's number)\n0) See your funds.\n1) Deposit money\n2) Withdraw money\n3) Manage your loans\n4) Transfer money to another account.", new IntegerInputChecker(0, 4)));
-		switch (userChoice) {
-			case 0 : {
-				_operations.seeYourFunds(account);
-				break;
+		try{
+			int userChoice = Integer.parseInt(_interface.askUser("What woud you like to do ?(Fill in with the option's number)\n0) See your funds.\n1) Deposit money\n2) Withdraw money\n3) Manage your loans\n4) Transfer money to another account.", new IntegerInputChecker(0, 4)));
+			switch (userChoice) {
+				case 0 : {
+					_operations.seeYourFunds(account);
+					break;
+				}
+				case 1 : {
+					_operations.depositMoney(account);
+					break;
+				}
+				case 2 : {
+					_operations.withdrawMoney(account);
+					break;
+				}
+				case 3 : {
+					_operations.manageLoans(account);
+					break;
+				}
+				case 4 : {
+					_operations.transferMoney(account);
+					break;
+				}
 			}
-			case 1 : {
-				_operations.depositMoney(account);
-				break;
-			}
-			case 2 : {
-				_operations.withdrawMoney(account);
-				break;
-			}
-			case 3 : {
-				_operations.manageLoans(account);
-				break;
-			}
-			case 4 : {
-				_operations.transferMoney(account);
-				break;
-			}
+		} catch (Exception e) {
+			System.out.println("failed");
 		}
 	}
 }
