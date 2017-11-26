1e5c4e589796fb9e0a205572c00e5057f1c0d74f
Soulthym <thymi92@gmail.com>
2017-11-26 14:55:19 +0100

    removed bad files

--- banking/src/banking/InputChecker.java
+++
@@ -1,7 +0,0 @@
-package banking;
-
-public interface InputChecker {
-
-	public void validate (String userInput) throws InputException;
-
-}
--- logger/src/logger/Logger.java
+++
@@ -1,8 +0,0 @@
-package logger;
-
-public interface Logger {
-
-	void info(String category, String message) throws WritingException;
-	void error(String category, String message)throws WritingException;
-
-}
